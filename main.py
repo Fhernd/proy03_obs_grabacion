284cdf559989853505579bd8b70deebf317e4f37
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-08-29 18:29:48 -0500

    Nos quedamos únicamente con el método de modificación de archivo

--- main.py
+++ main.py
@@ -3,33 +3,23 @@ import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-class MyHandler(FileSystemEventHandler):
+
+class GestorGrabacionObs(FileSystemEventHandler):
     def on_modified(self, event):
-        # Se detectó un cambio en un archivo o carpeta
         if event.is_directory:
             return
+        
         print(f'Archivo modificado: {event.src_path}')
 
-    def on_created(self, event):
-        # Se detectó la creación de un nuevo archivo o carpeta
-        if event.is_directory:
-            return
-        print(f'Archivo creado: {event.src_path}')
-
-    def on_deleted(self, event):
-        # Se detectó la eliminación de un archivo o carpeta
-        if event.is_directory:
-            return
-        print(f'Archivo eliminado: {event.src_path}')
-
-    # Puedes añadir más métodos para manejar otros tipos de eventos, como on_moved.
 
 if __name__ == "__main__":
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'  # Monitorea el directorio actual como predeterminado
-    event_handler = MyHandler()
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    
+    event_handler = GestorGrabacionObs()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
+    
     try:
         while True:
             time.sleep(1)
